428bd5ebe818672aba5bec2e96c6379ad78ca606
Fab <fhulous@gmail.com>
2026-09-01 19:00:39 +0200

    fix(StompAuthChannelInterceptor): return object was wrong

--- backend/src/main/java/com/ycyw/chatapi/configurations/StompAuthChannelInterceptor.java
+++ backend/src/main/java/com/ycyw/chatapi/configurations/StompAuthChannelInterceptor.java
@@ -6,6 +6,7 @@ import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -31,7 +32,10 @@ public class StompAuthChannelInterceptor implements ChannelInterceptor {
 
   @Override
   public Message<?> preSend(Message<?> message, MessageChannel channel) {
-    StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+    StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+    if (accessor == null || !accessor.isMutable()) {
+      accessor = StompHeaderAccessor.wrap(message);
+    }
 
     if (StompCommand.CONNECT.equals(accessor.getCommand())) {
       String token = extractBearerToken(accessor);
